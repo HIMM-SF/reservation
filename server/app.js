7744d7631906e67260cd7b43932443f9cb7d3d36
Marvin Caragay <mrvncaragay@gmail.com>
2020-03-11 18:46:12 -0700

    API GET /api/rooms/:id endpoint for retrieving a single room

--- server/app.js
+++ server/app.js
@@ -3,11 +3,22 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
 
+const Room = require('./rooms');
+
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.get('/api/rooms/:id', (req, res) => {
+  Room
+    .find({ _id: req.params.id })
+    .then(([ movie ]) => {
+      res.json(movie);
+    })
+    .catch(err => console.log(err));
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Listening to PORT ${PORT}`);
-})
+});
--- server/seedDB.js
+++ server/seedDB.js
@@ -2,31 +2,30 @@ const mongoose = require('mongoose');
 const Room = require('./rooms');
 const faker = require('faker');
 
+module.exports = () => {
+  const rooms = []
+  for(let i = 0; i < 100; i++) {
 
-const rooms = []
+    //Create random reviews. array of review object
+    let reviews = [];
+    for(let i = 0; i < faker.random.number({max: 30}); i++) {
+      reviews.push({
+        text: faker.lorem.words(),
+        userImg: faker.image.imageUrl(),
+        createAt: faker.date.past()
+      });
+    };
 
-for(let i = 0; i < 100; i++) {
-
-  //Create random reviews. array of review object
-  let reviews = [];
-  for(let i = 0; i < faker.random.number({max: 30}); i++) {
-    reviews.push({
-      text: faker.lorem.words(),
-      userImg: faker.image.imageUrl(),
-      createAt: faker.date.past()
+    rooms.push({
+      per_night: faker.finance.amount(),
+      reviews
     })
   }
 
-  rooms.push({
-    per_night: faker.finance.amount(),
-    reviews
+  Room.insertMany(rooms, err => {
+    if(err) console.log(err);
+
+    console.log('Successfully inserted data to DB...');
+    mongoose.connection.close();
   })
 }
-
-Room.insertMany(rooms, err => {
-  if(err) console.log(err);
-
-  console.log('Successfully insert data...');
-  mongoose.connection.close();
-})
-
