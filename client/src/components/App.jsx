e6b90802821643023cdb6161975f399828ec39b0
Marvin Caragay <mrvncaragay@gmail.com>
2020-03-12 00:04:43 -0700

    Init client

---
+++ client/src/components/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../styles/app.scss';
+
+const App = () => {
+  return (
+    <h1 className='main'>First Commit!</h1>
+  )
+};
+
+export default App; 
---
+++ client/webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src', 'index.js'),
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      }
+    ],
+  },
+  mode: 'development',
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  devServer: {
+    contentBase:  path.resolve(__dirname, 'public') // index.html
+  }
+};
