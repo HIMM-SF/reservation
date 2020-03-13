4b0baefc529da6912f2e3a0cb0b9122eeea41b5d
Marvin Caragay <mrvncaragay@gmail.com>
2020-03-12 20:42:50 -0700

    basic skeletal layout of reservation component

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -2,11 +2,36 @@ import React from "react";
 import "../stylesheets/app.scss";
 
 const App = () => (
-  <div className="header-container">
-    <header className="header">
-      Navigation Bar
-    </header>
-  </div>
+  <>
+    <div className="header-container">
+      <header className="header">
+        Navigation Bar
+      </header>
+    </div>
+
+    <div className="room-main">
+      <div className="room-images">
+        Images
+      </div>
+
+      <div className="room-main-body-container">
+        <div className="room-main-body">
+          <div className="room-main-body-left">
+            Left Content
+          </div>
+
+          <div className="room-main-body-right">
+            <div className="reservation-container">
+
+              <div className="reservation-component">
+                Component
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </>
 );
 
 export default App;
