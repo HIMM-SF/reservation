9e99e25d101f3dd6b315ce1b5802a074e8c00478
Marvin <mrvncaragay@gmail.com>
2020-03-13 18:21:37 -0700

    Revert "Revert "Html skeleton""

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -2,7 +2,36 @@ import React from "react";
 import "../stylesheets/app.scss";
 
 const App = () => (
-  <h1 className="main">First Commit!</h1>
+  <>
+    <div className="header-container">
+      <header className="header">
+        Navigation Bar
+      </header>
+    </div>
+
+    <div className="room-main">
+      <div className="room-images">
+        Images
+      </div>
+
+      <div className="room-main-body-container">
+        <div className="room-main-body">
+          <div className="room-main-body-left">
+            Left Content
+          </div>
+
+          <div className="room-main-body-right">
+            <div className="reservation-container">
+
+              <div className="reservation-component">
+                Component
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </>
 );
 
 export default App;
