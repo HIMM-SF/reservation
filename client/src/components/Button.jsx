4020662eb021e795b14fef6e6634c0fade30ec45
Marvin Caragay <mrvncaragay@gmail.com>
2020-03-25 14:34:38 -0700

    Add responsive CheckAvailability button and price

--- client/src/components/Button.jsx
+++ client/src/components/Button.jsx
@@ -6,8 +6,8 @@ const Button = styled.button`
   font-weight: 600;
   margin-top: 24px;
   cursor: pointer;
-  background-color: ${(props) => props.theme.btnBackgroundColor};
-  border: 1px solid ${(props) => props.theme.btnBackgroundColor};
+  background-color: ${(props) => props.theme.btnBackgroundColor2};
+  border: 1px solid ${(props) => props.theme.btnBackgroundColor2};
   border-radius: 5px;
 
   ${(props) => (props.block ? "display: block; width: 100%" : "")}
--- client/src/styled-components/theme.js
+++ client/src/styled-components/theme.js
@@ -2,6 +2,7 @@ export const strongFont = "#484848";
 export const lightFont = "#767676";
 export const fontBooked = "#bbbbbb";
 export const btnBackgroundColor = "#FF5A5F";
+export const btnBackgroundColor2 = "#e61e50";
 export const borderColorV2 = "#cccccc";
 export const borderColor = "#EBEBEB";
 export const checkIn = "#40969a";
